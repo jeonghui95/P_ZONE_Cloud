5446cb7e0b87111436bb020cc4584380e31a343f
smilemask92 <rhkwk3333@naver.com>
2023-01-16 16:05:03 +0900

    feat: 카메라 앱 연동
    db disconnect
    input으로 file request받음

--- P_ZONE_NOTICE/Camera_app/views.py
+++ P_ZONE_NOTICE/Camera_app/views.py
@@ -2,19 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import FileUploadForm
 from .models import FileUpload
-
+from PIL import Image
 def fileUpload(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        content = request.POST['content']
-        img = request.FILES["imgfile"]
-        fileupload = FileUpload(
-            title=title,
-            content=content,
-            imgfile=img,
-        )
-        fileupload.save()
-        return redirect('Camera_app:fileupload')
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        # img = request.FILES.get("imgfile")
+        img = request.FILES['Camera']
+        # fileupload = FileUpload(
+        #     title=title,
+        #     content=content,
+        #     imgfile=img,
+        # )
+        # fileupload.save()
+        # return redirect('Camera_app:fileupload')
+        # return HttpResponse(short)
+        img = Image.open(img)
+        img.show()
     else:
         fileuploadForm = FileUploadForm
         context = {
