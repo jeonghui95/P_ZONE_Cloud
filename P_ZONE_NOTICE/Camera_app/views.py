42cbb8d61009cbb87dca6a7be313d1889d2a7ebb
smilemask92 <rhkwk3333@naver.com>
2023-01-18 20:56:36 +0900

    feat: result.html 생성
    -response 해석하여 결과 도출
    
    fix: view.py 수정
    
    - model container에 request날리고 response 받는 함수 생성

--- P_ZONE_NOTICE/Camera_app/views.py
+++ P_ZONE_NOTICE/Camera_app/views.py
@@ -1,13 +1,16 @@
+import json
+
+import requests
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import FileUploadForm
-from .models import FileUpload
 from PIL import Image
+
 def fileUpload(request):
     if request.method == 'POST':
-        # title = request.POST.get('title')
+        # title = request.POST.get('title'  )
         # content = request.POST.get('content')
-        # img = request.FILES.get("imgfile")
+        # img = request.FILES.get("Camera")
         img = request.FILES['Camera']
         # fileupload = FileUpload(
         #     title=title,
@@ -16,12 +19,32 @@ def fileUpload(request):
         # )
         # fileupload.save()
         # return redirect('Camera_app:fileupload')
-        # return HttpResponse(short)
-        img = Image.open(img)
-        img.show()
+
+
+        url = 'http://fastmodel:8000/file/store'
+        upload = {'file': img}
+        context = requests.post(url,files=upload).json()
+        # m = MultipartEncoder(fields=upload)
+        # headers = {'Content-Type': m.content_type}
+        # res = requests.post(url, headers=headers, data=m)
+
+        # context = json.load(res)
+        return render(request,'result.html',context)
+
+
+
+
     else:
         fileuploadForm = FileUploadForm
         context = {
+
             'fileuploadForm': fileuploadForm,
         }
         return render(request, 'fileupload.html', context)
+# @csrf_exempt
+# def imageview(request):
+#     # return HttpResponse("HI")
+#     if request.method == 'POST':
+#         img = request.FILES["media"]
+#         img = Image.open(img)
+#         img.show()
