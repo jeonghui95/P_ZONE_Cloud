3de9734073341c8748e5d154d76fe90edd92e635
smilemask92 <rhkwk3333@naver.com>
2023-01-19 09:53:58 +0900

    fix: change debug

--- P_ZONE_NOTICE/P_ZONE_NOTICE/settings.py
+++ P_ZONE_NOTICE/P_ZONE_NOTICE/settings.py
@@ -56,7 +56,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
