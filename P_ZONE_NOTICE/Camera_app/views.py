a2a19f47b3c6e4b4afa5423e0cab99b357647c78
smilemask92 <rhkwk3333@naver.com>
2023-02-02 19:21:08 +0900

    fix: model 추가 marker update , qps 데이터 알림 추가

--- P_ZONE_NOTICE/Camera_app/views.py
+++ P_ZONE_NOTICE/Camera_app/views.py
@@ -4,37 +4,18 @@ import uuid
 from typing import IO
 import json
 
-
-import haversine as haversine
 import requests
-from haversine import haversine
 
 import pymysql
 import requests
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from P_ZONE_NOTICE.settings import MARIADB
 from .forms import FileUploadForm
-from tempfile import NamedTemporaryFile
-
-# def save_file(file: IO):
-#     # current = os.path.abspath(__file__).split("\\")
-#     # s3 업로드라고 생각해 봅시다. delete=True(기본값)이면
-#     # 현재 함수가 닫히고 파일도 지워집니다.
-#     UPLOAD_DIR = "./media"
-#     filename = f"{str(uuid.uuid4())}.png"  # uuid로 유니크한 파일명으로 변경
-#     with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
-#         fp.write(file.read())
-#         return filename
-#     #     tempfile = NamedTemporaryFile("wb", delete=False)
-#     #     tempfile.write(file.read())
-#     #     tempPath = tempfile.name.split("\\")[-1]
-#     #     return tempPath
 
 def fileUpload(request):
 
-
+    # DB requirement
     host = MARIADB['default']["DB_HOST"]
     user = MARIADB['default']["DB_USER"]
     password = MARIADB['default']["DB_PASSWORD"]
@@ -44,6 +25,8 @@ def fileUpload(request):
     cursor = connect.cursor()
 
 
+
+    # Request Post일때
     if request.method == 'POST':
         long = float(127.06139307841329)
         lat = float(37.509812901728836)
@@ -54,9 +37,12 @@ def fileUpload(request):
         sql = "select dongcode from dong"
         cursor.execute(sql)
         dong_list = [row[0] for row in cursor.fetchall()]
-
+        
+        
+        ## tmap appkey 설정 필요
+        # 현 위치데이터를 dong으로 변환 및 비교
         API = "https://apis.openapi.sk.com/tmap/geo/reversegeocoding?version=1"
-        payload = {"appKey": "l7xx0ffb87c22bdb45ae8facaeeb7958ad36", "lon": long, "lat": lat}  ## tmap appkey 설정 필요
+        payload = {"appKey": "l7xx0ffb87c22bdb45ae8facaeeb7958ad36", "lon": long, "lat": lat} 
         res = requests.get(API, params=payload)
         dongCode = int(json.loads(res.text)["addressInfo"]["legalDongCode"][5:8])
         if dongCode not in dong_list:
@@ -65,10 +51,26 @@ def fileUpload(request):
             print("여기는 강남구")
         print(dongCode)
 
+
+
+        ## MODEL 서버와 통신 후 Response
+        url = 'http://test-fastmodel:8000/file/store'
+        upload = {'file': img}
+
+        context = requests.post(url, files=upload).json()
+        # context = requests.post(url, files=upload)
+        # path= f"../../media/{save_file(img)}"
+        # image = Image.open(path)
+        # image.show()
+        context["lat"] = lat
+        context["long"] = long
+
+        
+        
+        # criteria를 기준으로 근처의 주차 금지구역 확인
         from haversine import haversine
         criteria = 100
         place_list = []
-        # 주차금지구역
         sql = f"""SELECT c.type, c.latitude, c.longitude
                   FROM cautionzone c
                   WHERE c.dongcode={dongCode}"""
@@ -79,7 +81,23 @@ def fileUpload(request):
                 print(row, distance)
                 place_list.append({"type": row[0], "latitude": row[1], "longitude": row[2]})
 
-        # 주차가능구역
+            if row[0] == "bus":
+                context["kinds"].append(4)
+                context["length"]=context["length"]+=1
+            elif row[0] == "fire":
+                context["kinds"].append(5)
+                context["length"]= context["length"]+= 1
+            elif row[0] == "taxi":
+                context["kinds"].append(6)
+                context["length"]= context["length"]+= 1
+            elif row[0] == "subway":
+                context["kinds"].append(7)
+                context["length"]= context["length"]+= 1
+            elif row[0] == "children":
+                context["kinds"].append(8)
+                context["length"]= context["length"]+= 1
+
+        #  // 주차가능구역 확인
         sql = f"""SELECT p.type, p.latitude, p.longitude
                   FROM parkingzone p
                   WHERE p.dongcode={dongCode}"""
@@ -90,42 +108,14 @@ def fileUpload(request):
                 print(row)
                 place_list.append({"type": row[0], "latitude": row[1], "longitude": row[2]})
 
-
-
-
-        # image
-        # fileupload = FileUpload(
-        #     title=title,
-        #     content=content,
-        #     imgfile=img,
-        # )
-        # fileupload.save()
-        # return redirect('Camera_app:fileupload')
-
-
-        url = 'http://test-fastmodel:8000/file/store'
-        upload = {'file': img}
-
-        context = requests.post(url,files=upload).json()
-        # context = requests.post(url, files=upload)
-        # path= f"../../media/{save_file(img)}"
-        # image = Image.open(path)
-        # image.show()
-        context["lat"] = lat
-        context["long"] = long
         context["placeList"] = place_list
-        print(context)
-        # m = MultipartEncoder(fields=upload)
-        # headers = {'Content-Type': m.content_type}
-        # res = requests.post(url, headers=headers, data=m)
 
-        # context = json.load(res)
 
         return  render(request,'result.html',context)
 
 
 
-
+    ## request method가 Get일때 (Home 페이지 html 호출)
     else:
         fileuploadForm = FileUploadForm
         context = {
@@ -133,10 +123,3 @@ def fileUpload(request):
             'fileuploadForm': fileuploadForm,
         }
         return render(request, 'fileupload.html', context)
-# @csrf_exempt
-# def imageview(request):
-#     # return HttpResponse("HI")
-#     if request.method == 'POST':
-#         img = request.FILES["media"]
-#         img = Image.open(img)
-#         img.show()
